77f0542d6582b7689a2ebb346f9f2c5036717c39
Noel Mendoza <nmendozavd@gmail.com>
2018-01-18 12:12:20 -0800

    Initial Commit

---
+++ North East West South/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  North East West South
+//
+//  Created by Noel Mendoza on 1/18/18.
+//  Copyright © 2018 Noel Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
